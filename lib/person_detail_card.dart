97cc2203a88bc643eb874bdf5a7ea6c35050be99
luke choi <primera_jh@hotmail.com>
2021-12-13 23:34:39 +0900

    feed page done

--- lib/person_detail_card.dart
+++ lib/person_detail_card.dart
@@ -10,7 +10,8 @@ class PersonDetailCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color:Colors.white,
-      elevation: 0.0,
+      elevation: 50.0,
+      margin: const EdgeInsets.fromLTRB(18, 10, 18, 10),
       shadowColor: Colors.grey.withOpacity(0.5),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
@@ -26,10 +27,56 @@ class PersonDetailCard extends StatelessWidget {
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 border: Border.all(width:5,color: Colors.white),
-                image: DecorationImage(image: AssetImage(person.img))
-
+                image: DecorationImage(
+                    image: AssetImage(person.img),
               ),
-            )
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 7,
+                    spreadRadius: 8,
+                    color:Colors.grey,
+                    offset: Offset(0,3),
+                  )
+                ]
+              )
+            ),
+            const SizedBox(width:20),
+            Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(padding: const EdgeInsets.only(top: 8, bottom:8,),
+                child: Text(person.name, style: const TextStyle(fontSize: 24,),
+                ),
+                ),
+                Padding(padding: const EdgeInsets.only(top: 0, bottom:0,),
+                  child: Text(person.job, style: const TextStyle(fontSize: 20,color: Colors.grey),
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.only(top:8,bottom: 8),
+                child:Row(
+                  children: [
+                    const Icon(Icons.access_time_outlined,size:20),
+                    const SizedBox(width:10),
+                    Text(person.clock, style:const TextStyle(fontSize: 18,color: Colors.grey))
+                  ],
+                ),)
+              ],
+            ),
+            Expanded(child:
+            Row(
+              children: [
+                Expanded(child: Container(),),
+                Column(
+                  children: const [
+                    Icon(Icons.circle,size:8),
+                    SizedBox(height: 5,),
+                    Icon(Icons.circle,size:8),
+                    SizedBox(height: 5,),
+                    Icon(Icons.circle,size:8)],
+                )
+              ],
+            ))
           ],
         ),
       ),
